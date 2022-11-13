0d46fc9f8644126bf869cf33637b32b20cd99c28
LunarLambda <lunarlambda@gmail.com>
2022-11-13 14:20:26 +0100

    gba_base.h: Fix GCC section conflict error
    Make the *_CODE and *_DATA section macros use separate section names.
    This fixes GCC complaining about conflicting section permissions
    (R-X vs RW-) when IWRAM_CODE and IWRAM_DATA are used in the same file.
    See https://github.com/devkitPro/libgba/pull/9 for more information.

--- include/gba_base.h
+++ include/gba_base.h
@@ -78,7 +78,7 @@
 
     \brief helper macro to insert a bios call.
 		\param Number swi number to call
-		
+
 		Inserts a swi of the correct format for arm or thumb code.
 
 */
@@ -96,11 +96,11 @@
 */
 #define BIT(number) (1<<(number))
 
-#define IWRAM_CODE	__attribute__((section(".iwram"), long_call))
-#define EWRAM_CODE	__attribute__((section(".ewram"), long_call))
+#define IWRAM_CODE	__attribute__((section(".iwram.text"), long_call))
+#define EWRAM_CODE	__attribute__((section(".ewram.text"), long_call))
 
-#define IWRAM_DATA	__attribute__((section(".iwram")))
-#define EWRAM_DATA	__attribute__((section(".ewram")))
+#define IWRAM_DATA	__attribute__((section(".iwram.data")))
+#define EWRAM_DATA	__attribute__((section(".ewram.data")))
 #define EWRAM_BSS	__attribute__((section(".sbss")))
 #define ALIGN(m)	__attribute__((aligned (m)))
 
